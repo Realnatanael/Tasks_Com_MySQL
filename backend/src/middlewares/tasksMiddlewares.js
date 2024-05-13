a64abae2746594688ee5d700aebe79938e443e98
Natanael Nascimento <Natanaelrealamazonas@gmail.com>
2024-05-13 01:51:48 -0400

    feat: add validation for fields in tasksMiddlewares
    The code changes in `tasksMiddlewares.js` add a new middleware function `validateFields` to validate the `title` and `status` fields in the request body. This ensures that both fields are present and not empty before proceeding with the request.

--- backend/src/middlewares/tasksMiddlewares.js
+++ backend/src/middlewares/tasksMiddlewares.js
@@ -3,3 +3,10 @@ export const validateBody  = (req, res, next) => {
     if(!title || title === "" || title === undefined) return res.status(400).json({message: 'O campo "title" é obrigatório'}); 
     next();
 }
+
+export const validateFields = (req, res, next) => {
+    const { title, status } = req.body;
+    if(!title || title === "" || title === undefined) return res.status(400).json({message: 'O campo "title" é obrigatório'}); 
+    if(!status || status === "" || status === undefined) return res.status(400).json({message: 'O campo "status" é obrigatório'}); 
+    next();
+}
--- backend/src/routers/router.js
+++ backend/src/routers/router.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import {create, deleted, getAll, update} from "../controllers/tasksControllers.js";
-import { validateBody } from "../middlewares/tasksMiddlewares.js";
+import { validateBody, validateFields } from "../middlewares/tasksMiddlewares.js";
 
 export const router = Router();
 
 router.get('/tasks', getAll);
 router.post('/tasks', validateBody, create);
 router.delete('/tasks/:id', deleted);
-router.put('/tasks/:id', validateBody, update); //o update é o mesmo que o create, só que com o método put
+router.put('/tasks/:id', validateFields, update); //o update é o mesmo que o create, só que com o método put
